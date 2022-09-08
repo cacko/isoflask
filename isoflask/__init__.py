2c2705f4b396cecef8d2f33fbe63cefc746c245c
cacko <alex@cacko.net>
2022-09-08 04:47:03 +0100

    Give me a break, it's 2am.  But it works now.

--- isoflask/__init__.py
+++ isoflask/__init__.py
@@ -1,22 +1,27 @@
 from datetime import datetime
 from flask.json.provider import JSONProvider
 import json
-from pathlib import Path
+from pathlib import
 from flask import Flask
+from pathlib import Path
 
-__name__ = "isoflask"
+__name__ = 'isoflask'
 
 
 class ISOJSONProvider(JSONProvider):
-    def default(self, obj):
+    def dumps(self, obj):
         if isinstance(obj, Path):
             return obj.as_posix()
         elif isinstance(obj, datetime):
             return obj.isoformat()
         return json.JSONEncoder.default(self, obj)
 
+    def loads(self, s, **kwargs):
+        return json.loads(s)
+
 
 class ISOFlask(Flask):
+
     def __init__(self, *args, **kwds):
         super().__init__(*args, **kwds)
         self.json = ISOJSONProvider(self)
