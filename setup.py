556a24b7515e809d7d81ade28717036c016ad2f6
cacko <alex@cacko.net>
2022-09-08 04:40:41 +0100

    add actual words

--- setup.py
+++ setup.py
@@ -1,23 +1,31 @@
 from setuptools import setup, find_packages
 from setuptools.dist import Distribution as _Distribution
-from isoflask.version import __version__
 from pathlib import Path
 import semver
 from isoflask import __name__
+import sys
 
 
-def inc_version():
-    init = Path(__file__).parent / __name__ / "version.py"
-    _, v = init.read_text().split(' = ')
-    cv = semver.VersionInfo.parse(v.strip('"'))
-    init.write_text(f'__version__ = "{cv.bump_patch()}"')
+
+def version():
+    if len(sys.argv) > 1 and sys.argv[1] >= "bdist_wheel":
+        init = Path(__file__).parent / __name__ / "version.py"
+        _, v = init.read_text().split(' = ')
+        cv = semver.VersionInfo.parse(v.strip('"'))
+        nv = f"{cv.bump_patch()}"
+        init.write_text(f'__version__ = "{nv}"')
+        print(nv)
+        return nv
+    from isoflask.version import __version__
+    return __version__
+
 
 class Distribution(_Distribution):
     def is_pure(self): return True
 
 setup(
     name=__name__,
-    version=__version__,
+    version=version(),
     author='cacko',
     author_email='alex@cacko.net',
     distclass=Distribution,
